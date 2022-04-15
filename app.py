a70de1afa4bea09b073629e8afaeaa62f8332611
songyu <songyu@elibot.cn>
2022-04-15 10:41:52 +0800

    Added upload port '/uploadapi'

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template, current_app
 from werkzeug.utils import secure_filename
-from flask import send_from_directory
+from flask import send_from_directory, jsonify
 import random, time
 import db
 import getConfig as gcf
+import json
 
 from flask.cli import with_appcontext
 
@@ -66,6 +67,51 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+@app.route('/uploadapi', methods=['POST', 'GET'] )
+def upload_file_api():
+    app.logger.info('this is url')
+    if request.method == 'POST':
+        # 检查post请求中是否有文件
+        if 'file' not in request.files:
+            flash('你没有上传文件！')
+            return redirect(request.url)
+        file = request.files['file']
+        print(file)
+        imgUrl = ''
+        if file.filename == '':
+            flash('你没有选择文件！')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = str(int(time.time())) + str(random.randint(1, 99999)) + secure_filename(str(random.randint(1, 7887)) + file.filename)
+            try:
+                file.save(os.path.join(upload_folder, filename))
+                database = db.get_db()
+                database.execute(
+                    'INSERT INTO pics (filename)'
+                    ' VALUES (?)',
+                    (filename,)
+                )
+                database.commit()
+                if app.config['running_port'] != 80:
+                    imgUrl = app.config['running_domain'] + ':' + str(app.config['running_port']) + url_for('uploaded_file', filename=filename)
+                    flash(imgUrl)
+                else:
+                    imgUrl = app.config['running_domain'] + url_for('uploaded_file', filename=filename)
+                    flash(imgUrl)
+            except Exception as e:
+                flash('出现错误！')
+                print(e.args)
+ 
+            return jsonify({'data': 'http://'+imgUrl}),200
+        else:
+            flash('不被服务器支持的文件！')
+            return redirect(url_for('upload_file'))
+    database = db.get_db()
+    pcnum = database.execute("SELECT Count(*) FROM pics").fetchone()[0]
+    print(pcnum)
+    # print('this is url')
+    # print(json.dumps({'data': imgUrl}))
+    # return jsonify({'data': imgUrl}),200
 
 if __name__ == '__main__':
 
